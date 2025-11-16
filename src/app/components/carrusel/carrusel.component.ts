a105b6e8e4ef344fcc87105e1f5f2058104e3469
IsaacAuditore7037 <131411886+IsaacAuditore7037@users.noreply.github.com>
2025-11-16 13:18:20 -0600

    Modificacion

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -1,55 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
-  styleUrl: './carrusel.component.css'
+  styleUrls: ['./carrusel.component.css']
 })
-export class CarruselComponent implements OnInit{
+export class CarruselComponent implements OnInit {
 
-  constructor(
-    public _Router: Router,
-  ) {}
+  @ViewChild('bgMusic') bgMusic!: ElementRef<HTMLAudioElement>;
 
-  images: any [] = [];
+  hearts: any[] = [];
+  images: any[] = [];
+  musicStarted = false;
 
+  constructor(public _Router: Router) {}
 
   ngOnInit(): void {
-
-    let numeros: number [] = [];
-
-    // for(let i = 1; i < 21; i++){
-    //   let numero: number = 0;
-    //   numero = Math.floor(Math.random() * (20 - 1) + 0);
-    //   numeros.push(numero);
-    // }
-
-    for(let i = 1; i < 29; i++){
-      // let numero: number = 0;
-      // numero = Math.floor(Math.random() * (20 - 1) + 0);
-      // numeros.push(numero);
-      numeros.push(i);
+    // Inicializar collage
+    for (let i = 1; i < 31; i++) {
+      this.images.push("../../../assets/Collage/" + i + ".jpg");
     }
 
-    console.log("numeros", numeros);
+    // Intentar reproducir música
+    // setTimeout(() => {
+    //   const audio = this.bgMusic?.nativeElement;
+    //   if (audio) {
+    //     audio.volume = 0.5;
+    //     audio.play().catch(() => {
+    //       console.log("El navegador bloqueó la reproducción automática de audio");
+    //     });
+    //   }
+    // }, 500); // Pequeño delay para garantizar carga de DOM
 
-    numeros.forEach((numero) => {
-      // if(numero == 0){
-      //   this.images.push("https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg");
-      // } else {
-      //   this.images.push("https://flowbite.s3.amazonaws.com/docs/gallery/square/image-" + numero +".jpg");
-      // }
-      this.images.push("../../../assets/Collage/" + numero +".jpg")
+
+    this.generateHearts(); // iniciar generación de corazones
+  }
+
+   generateHearts() {
+    setInterval(() => {
+      const heart = {
+        id: Math.random(),
+        left: Math.random() * 100,        // posición horizontal %
+        size: 20 + Math.random() * 40,    // tamaño 20-60px
+        duration: 4 + Math.random() * 4,  // duración 4-8s
+        color: `hsla(${330 + Math.random() * 30}, 100%, 75%, ${0.5 + Math.random()*0.5})`, // tonos rosados
+        rotate: Math.random() * 360        // rotación inicial
+      };
+      this.hearts.push(heart);
+
+      // Limitar la cantidad de corazones en el DOM
+      if (this.hearts.length > 150) {
+        this.hearts.shift();
+      }
+    }, 200); // cada 200ms aparece un corazón
+  }
+
+playMusic() {
+  const audio = this.bgMusic?.nativeElement;
+  if (audio) {
+    audio.volume = 0.5;
+    audio.play().then(() => {
+      this.musicStarted = true; // ocultar overlay
+    }).catch(() => {
+      console.log("El navegador bloqueó la reproducción automática de audio");
     });
-
-    console.log("images", this.images);
-
   }
+}
 
-  letter(){
-    let path = `/letter`;
-    this._Router.navigate([path]);
+previewImage: string | null = null;
+
+showPreview(image: string) {
+  this.previewImage = image;
+}
+
+hidePreview() {
+  this.previewImage = null;
+}
+
+  letter() {
+    this._Router.navigate(['/letter']);
   }
-
 }
